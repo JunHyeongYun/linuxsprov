4188d6d815bdc7edb6d459dcbaef7a02c9264f98
JunHyeongYun <aapp7979@daum.net>
2026-06-15 12:37:06 +0900

    Initial commit

---
+++ src/main/java/com/example/linuxsprov/LinuxsprovApplication.java
@@ -0,0 +1,13 @@
+package com.example.linuxsprov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxsprovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxsprovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/linuxsprov/LinuxsprovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linuxsprov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxsprovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
